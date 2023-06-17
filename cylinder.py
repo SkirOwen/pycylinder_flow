dbeee6061bc50c5e9e6828ef231491189366d32c
Owen <owen.allemang@gmail.com>
2023-06-17 20:18:52 +0100

    fixed blank line in the copyright notice

--- cylinder.py
+++ cylinder.py
@@ -27,18 +27,18 @@
 ##################################################################
 
 # Copyright (C) 2023  Owen Allemang
-
+#
 # This file is part of PyCylinder_flow.
 # PyCylinder_flow is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
-
+#
 # PyCylinder_flow is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License along with Foobar.
 # If not, see <https://www.gnu.org/licenses/>.
 
@@ -46,9 +46,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.io
+from PIL import Image
 from tqdm import tqdm
 
-from PIL import Image
 import cmocean as cm
 
 from utils import clear_dir
--- utils.py
+++ utils.py
@@ -1,19 +1,20 @@
 # Copyright (C) 2023  Owen Allemang
-import glob
+#
 # This file is part of PyCylinder_flow.
 # PyCylinder_flow is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, either version 3 of the License,
 # or (at your option) any later version.
-
+#
 # PyCylinder_flow is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU General Public License for more details.
-
+#
 # You should have received a copy of the GNU General Public License along with Foobar.
 # If not, see <https://www.gnu.org/licenses/>.
 
+import glob
 import os
 
 
@@ -40,3 +41,5 @@ def clear_dir(target: str, extension: str):
 	files_to_delete = glob.glob(os.path.join(target, file_pattern))
 	for file in files_to_delete:
 		os.remove(file)
+
+
